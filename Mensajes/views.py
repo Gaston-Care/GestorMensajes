1b4c353f3d6033d6003c85cea708279a9a19678b
Gaston-Care <gecare@udc.edu.ar>
2024-08-23 15:29:47 -0300

    Añado la funcion filtrando por destinatario en views.py

--- Mensajes/views.py
+++ Mensajes/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Mensaje
 
 # Create your views here.
 def ver_mensajes_recibidos(request):
-    return HttpResponse("hola")
+    mensajes_recibidos = Mensaje.objects.filter(destinatario='Gaston')
+    return render(request, 'mensajes.html', {'mensajes': mensajes_recibidos})
